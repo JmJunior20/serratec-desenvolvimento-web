2aaf085cd797f748afab3c5067f22523656932ef
Junior Monteiro <jmjuniormonteiro@gmail.com>
2021-06-15 11:44:55 -0300

    Aula01

--- Aula01_150621/index.js
+++ Aula01_150621/index.js
@@ -23,3 +23,49 @@ function retornarSeParOuImpar(numero) {
 
 //dispararAlerta();
 //dispararAlerta2("Bem vindos de volta ;)");
+
+//==================//================//
+
+/* // Variável com escopo global
+let nome = "Fulano";
+
+function escreverNomeNaTela() {
+    //É um escopo de função ou local
+    var nome = "Ciclano";
+    //var sobreNome = " da Silva";
+    document.write(nome);
+}
+
+if(true) {
+    // Aqui é um escopo de bloco.
+    let nome = "Beltrano";
+    document.write(nome);
+}
+
+//escreverNomeNaTela();
+document.write(nome); */
+
+//==================//================//
+//Arrays
+
+let nomes = [];
+
+// Serve para add um novo elemento no array.
+nomes.push("Fulano");
+
+nomes[0]
+
+/*
+Ganhar dominio nesse inicio.
+
+Dominar Array (push, filter, find, findIndex, forEach e o map).
+Diferenca entre escopos (Global, Função e Bloco).
+Referencia de objetos.
+
+Links: para estudo
+https://www.w3schools.com/js/js_array_methods.asp
+https://www.w3schools.com/js/js_arrays.asp
+https://www.w3schools.com/js/js_let.asp
+https://www.w3schools.com/js/js_scope.asp
+
+*/
