183390bfdd645e86b4fd889a4c2ae752599bfee6
Junior Monteiro <jmjuniormonteiro@gmail.com>
2021-06-16 13:11:21 -0300

    Exercicios 1 - Criando o header da Padaria

--- Aula02_160621/padaria-do-seu-ze/src/App.js
+++ Aula02_160621/padaria-do-seu-ze/src/App.js
@@ -1,6 +1,13 @@
+import Header from './components/Header';
+
+import './styles.css';
+
 function App() {
   return (
     <div className="App">
+      <Header></Header>
+      <h1>Primeira Aplicação</h1>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
     </div>
   );
 }
