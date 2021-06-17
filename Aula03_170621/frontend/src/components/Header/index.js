1abee598be1b823bbfc5ae6ef41628e1c72e598b
Junior Monteiro <jmjuniormonteiro@gmail.com>
2021-06-17 09:43:51 -0300

    Subindo projeto

---
+++ Aula03_170621/frontend/src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+// Construindo um componente em formato de função
+// é chamado de Stalles
+const Header = () => (
+    <div className="header">Padaria do seu Zé</div>
+);
+
+export default Header;
---
+++ Aula03_170621/frontend/src/pages/principal/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default class Principal extends React.Component {
+
+}
