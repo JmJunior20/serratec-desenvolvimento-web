fa3cbb9223a02015252862ed5499618a103b3815
Junior Monteiro <jmjuniormonteiro@gmail.com>
2021-06-17 12:00:36 -0300

    Criando a div de produtos

--- Aula03_170621/frontend/src/App.js
+++ Aula03_170621/frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Principal from './pages/principal'
 import "./style.css";
 
 function App() {
   return (
     <div className="container">
       <Header></Header>
+      <Principal></Principal>
     </div>
   );
 }
--- Aula03_170621/frontend/src/pages/principal/index.js
+++ Aula03_170621/frontend/src/pages/principal/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import './index.css'
 
 export default class Principal extends React.Component {
+    state = {
+        produtos: [],
+        mensagem: "Ola!!!"
+    }
 
+    render() {
+        return (
+            <div className="container-produtos">
+                
+                {this.state.produtos.map(produto => (
+                    <div className="produtos" key={ produto.id }>
+                        <p><strong>Id:</strong> { produto.id }</p>
+                        <p><strong>Nome:</strong> { produto.nome }</p>
+                        <p><strong>Valor:</strong> { produto.valor }</p>
+                    </div>
+                ))}               
+
+            </div>            
+        );
+    }
 }
